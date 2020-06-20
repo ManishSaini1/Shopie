78f7412f383a7fe4ee2a1e26b2056efa75c2b05e
Manish Saini <mk81895@gmail.com>
2020-06-21 02:01:32 +0530

    Dynamic Amount and Cart Total

--- src/App.js
+++ src/App.js
@@ -3,13 +3,99 @@ import './App.css';
 import Cart from './Cart';
 import Navbar from './Navbar';
 
-function App() {
+class App extends React.Component {
+   constructor()
+  {
+    super();
+    this.state={
+     products :[
+         {
+            price :9999,
+            title: "Mobile Phone",
+            qty: 1,
+            img: 'https://images.unsplash.com/photo-1567581935884-3349723552ca?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=967&q=80',
+            id:1
+         }, 
+         {
+            price :999,
+            title: "Watch",
+            qty: 1,
+            img: 'https://images.unsplash.com/photo-1523170335258-f5ed11844a49?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1059&q=80',
+            id:2
+         },
+         {
+            price :39999,
+            title: "Laptop",
+            qty: 1,
+            img: 'https://images.unsplash.com/photo-1548611635-b6e7827d7d4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+            id:3
+         }
+     ]
+    }
+  }
+  handleIncreaseQuantity= (product)=>{
+      const {products}= this.state;
+      const index= products.indexOf(product);
+      products[index].qty+=1;
+      this.setState({
+          products
+      })
+  }
+  handleDecreaseQuantity= (product)=>{
+    const {products}= this.state;
+    const index= products.indexOf(product);
+    if(products[index].qty===0)
+    {
+        return;
+    }
+    products[index].qty-=1;
+    this.setState({
+        products
+    })
+}
+handleDeleteProduct=(id)=>{
+    const{products}=this.state;
+    const items= products.filter((item)=>item.id!==id);
+    this.setState({
+        products : items
+    });
+}
+getCarCount=()=>{
+    const {products}= this.state;
+    let count=0;
+    products.forEach((product)=>
+    {
+      count+=product.qty;
+    });
+    return count;
+}
+getCartTotal=()=>
+{
+  const{products}=this.state;
+  let total=0;
+  products.forEach((product)=>
+  {
+    total+=product.price * product.qty
+
+  });
+  return total;
+}
+  render()
+  {
+    const {products} =this.state;
   return (
     <div className="App">
-      <Navbar />
-       <Cart />
+      <Navbar count={this.getCarCount()} />
+       <Cart
+       products  ={products}
+       onIncreaseQuantity= {this.handleIncreaseQuantity}
+       onDecreaseQuantity= {this.handleDecreaseQuantity}
+       onDeleteProduct= {this.handleDeleteProduct}
+      />
+      <div style={{padding:20 , backgroundColor: '#AB6437', width: 'content-fit' }}>Total : {this.getCartTotal()}</div>
     </div>
   );
+}
 }
 
 export default App;
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,7 +1,56 @@
 import React from 'react';
 
-class CartItem extends React.Component {
-  // constructor()
+const CartItem = (props)=> {
+    const {title, price, qty}= props.product;
+    const {product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct} = props;
+    return (
+      <div className="cart-item">
+        <div className="left-block" >
+          <img style={styles.image} src={product.img} alt="prod imag" />
+        </div>
+        <div className="right-block">
+    <div style={ { fontSize: 25 } }>{title}</div>
+    <div style={ { color: '#777' } }>Rs {price}</div>
+          <div style={ { color: '#777' } }>Qty: {qty}</div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
+            <img 
+            alt="plus" 
+            className="action-icons" 
+            src="https://image.flaticon.com/icons/svg/992/992651.svg"
+            onClick= {() =>onIncreaseQuantity(product)}
+          />
+          
+            <img 
+            alt="minus" 
+            className="action-icons" 
+            src="https://image.flaticon.com/icons/svg/864/864373.svg"
+             onClick= {()=>onDecreaseQuantity(product)} 
+            />
+            <img 
+            alt="delete" 
+            className="action-icons" 
+            src="https://image.flaticon.com/icons/svg/3096/3096687.svg"
+            onClick={()=> onDeleteProduct(product.id)}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
+const styles = {
+  image: {
+    height: 150,
+    width: 130,
+    borderRadius: 4,
+    background: '#ccc'
+  }
+}
+
+export default CartItem;
+// constructor()
   // {
   //   super();
   //   this.state={
@@ -36,53 +85,3 @@ class CartItem extends React.Component {
     //     }
     //   });
     // console.log(this.state);
-  render () {
-    const {title, price, qty}= this.props.product;
-    const {product, onIncreaseQuantity, onDecreaseQuantity, onDeleteProduct} = this.props;
-    return (
-      <div className="cart-item">
-        <div className="left-block" style={styles.image}>
-          <img style={styles.image} alt="h"  />
-        </div>
-        <div className="right-block">
-    <div style={ { fontSize: 25 } }>{title}</div>
-    <div style={ { color: '#777' } }>Rs {price}</div>
-          <div style={ { color: '#777' } }>Qty: {qty}</div>
-          <div className="cart-item-actions">
-            {/* Buttons */}
-            <img 
-            alt="plus" 
-            className="action-icons" 
-            src="https://image.flaticon.com/icons/svg/992/992651.svg"
-            onClick= {() =>onIncreaseQuantity(product)}
-          />
-          
-            <img 
-            alt="minus" 
-            className="action-icons" 
-            src="https://image.flaticon.com/icons/svg/864/864373.svg"
-             onClick= {()=>onDecreaseQuantity(product)} 
-            />
-            <img 
-            alt="delete" 
-            className="action-icons" 
-            src="https://image.flaticon.com/icons/svg/3096/3096687.svg"
-            onClick={()=> onDeleteProduct(product.id)}
-            />
-          </div>
-        </div>
-      </div>
-    );
-  }
-}
-
-const styles = {
-  image: {
-    height: 130,
-    width: 110,
-    borderRadius: 4,
-    background: '#ccc'
-  }
-}
-
-export default CartItem;
