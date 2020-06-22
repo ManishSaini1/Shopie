9a0c13f780bfe22c6a3bee49a5ab79bce2a2439a
Manish Saini <42988015+ManishSaini1@users.noreply.github.com>
2020-06-23 00:24:36 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,15 +6,15 @@ import * as serviceWorker from './serviceWorker';
 import * as firebase from 'firebase';
 import 'firebase/firestore'
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB-Vum17rDD7VESG0Xg9lu_wHUWK_W-fbM",
-  authDomain: "cart-2f05c.firebaseapp.com",
-  databaseURL: "https://cart-2f05c.firebaseio.com",
-  projectId: "cart-2f05c",
-  storageBucket: "cart-2f05c.appspot.com",
-  messagingSenderId: "784450824539",
-  appId: "1:784450824539:web:ae134b205df817c8f6927e"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyB-Vum17rDD7VESG0Xg9lu_wHUWK_W-fbM",
+//   authDomain: "cart-2f05c.firebaseapp.com",
+//   databaseURL: "https://cart-2f05c.firebaseio.com",
+//   projectId: "cart-2f05c",
+//   storageBucket: "cart-2f05c.appspot.com",
+//   messagingSenderId: "784450824539",
+//   appId: "1:784450824539:web:ae134b205df817c8f6927e"
+// };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 ReactDOM.render(
